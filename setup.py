b3edb33a2a0161663ac0580caf6d4cc186c32620
opencm <limor@gigaspaces.com>
2015-03-11 16:21:10 +0200

    Bump version to 1.2m7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-libcloud-plugin',
-    version='1.2a6',
+    version='1.2a7',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=[
@@ -32,7 +32,7 @@ setup(
     license='LICENSE',
     description='Cloudify plugin for Libcloud infrastructure.',
     install_requires=[
-        'cloudify-plugins-common>=3.2a6',
+        'cloudify-plugins-common>=3.2a7',
         'apache-libcloud==0.15.1',
     ]
 )
